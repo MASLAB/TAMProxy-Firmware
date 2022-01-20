b88365fa7c4ca01892a675e1e0f7e8de8df3dc8a
johnzzhang <johnz@mit.edu>
2022-01-20 13:32:39 -0500

    Tuned PI loop for position.

--- src/Const.h
+++ src/Const.h
@@ -1,6 +1,6 @@
 #define LOOP_TIME 1000 //!< loop time in microseconds for controller
-#define DEF_PID_POS_K 0.2f //!< default PID controller K value
-#define DEF_PID_POS_TI 1.0e6f //!<  default PID controller Ti value
+#define DEF_PID_POS_K 1.0f //!< default PID controller K value
+#define DEF_PID_POS_TI 2.0f //!<  default PID controller Ti value
 #define DEF_PID_POS_LIMIT 1.0f //!< default PID controller PWM limit
 #define DEF_POS_FILTER_Tf 0.005f //!< default filter time constant
 
