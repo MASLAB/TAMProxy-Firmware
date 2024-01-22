9cb1bfd54f6651e2f8bb6458bea65e916de80ee2
Joseph Hobbs <josephrosshobbs@gmail.com>
2024-01-22 13:27:39 -0500

    Changed estimated velocity to encoder count

--- src/FeedbackMotor.cpp
+++ src/FeedbackMotor.cpp
@@ -50,17 +50,17 @@ std::vector<uint8_t> FeedbackMotor::handleRequest(std::vector<uint8_t> &request)
             desiredAngle = f;
         }
     }
-    
+
     if (request[0] == ENCODER_READ_CODE) {
         if (request.size() != 1) return {REQUEST_LENGTH_INVALID_CODE};
 
         if (velocityControl) {
             // send current estimated velocity
             union {
-                float f;
+                int32_t c;
                 uint8_t bytes[4];
             } val;
-            val.f = currentVelocity;
+            val.c = encCount;
 
             return {
                 static_cast<uint8_t>(val.bytes[0]),
